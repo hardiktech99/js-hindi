2989626254293cf20c07e6b7f8bcd7df7f96bde7
hardiktech99 <Hardikpatel4314@gmail.com>
2025-01-20 06:59:26 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer varsion
+
+// alert(3+5) we are using nodejs, not browser
+
+console.log(3 + 60 + (6-3));
+console.log("my name is hardik")
+
+let name = "hardik"
+let age = 29
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+//bigint 
+// string => ""
+// boolean => true/false
+// null  => stndalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // underfined
+console.log(typeof null); // object
