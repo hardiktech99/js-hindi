3d31e330f178bf14647acf38708bc164269dd4f4
hardiktech99 <Hardikpatel4314@gmail.com>
2025-01-20 08:22:53 +0000

    updated conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,7 +21,46 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 
-let stringNumber = String (someNumber)
-console.log (stringNumber);
-console.log(typeof stringNumber);
+let stringNumber = String(someNumber)
+// console.log (stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********************************** Operations ******************************************
+
+let value = 6
+let nagValue = -value
+// console.log(nagValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%5);
+
+let str1 = "hello"
+let str2 = "hardik"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 6);
+// console.log(5 + "5");
+// console.log("5" + (5 + 5));
+// console.log(8 + 8 + "8");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++ gameCounter;
+console.log(gameCounter);
+
+// study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
 
