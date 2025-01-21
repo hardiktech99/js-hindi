97f3e08a5d5b69bda8ba54bbeafe0ed1e0d86bfe
hardiktech99 <hardikpatel4314@gmail.com>
2025-01-21 17:13:38 +0530

    add iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,9 @@
+// Immediately Imvoked Funcation Expression (IIFE)
+
+(function chai(){
+    console.log(`DB CONNECTED`);
+}) ();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})("Hardik")
