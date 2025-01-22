06670f2a202a10a85179a7212a883f7d9353eb16
hardiktech99 <hardikpatel4314@gmail.com>
2025-01-22 14:47:32 +0530

    add loop file

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+// for iN loop --------------------------------------------------------------
+
+
+const myObject = {
+    js : "Javascript",
+    cpp : "c++",
+    rb : "ruby",
+    swift : "Swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js","rb","py","java","cpp"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+    
+}
+
+// const map = new Map()
+// map.set("IN" , "India")
+// map.set("US" , "USA")
+// map.set("Fr" , "France")
+
+// for (const key in map) {
+//     console.log(key);
+// }
