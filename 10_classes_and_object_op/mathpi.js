f4e85f57bca47e333850ffa59dac3588296b7b6d
hardiktech99 <hardikpatel4314@gmail.com>
2025-01-28 17:47:58 +0530

    add Mathpi file

--- 10_classes_and_object_op/mathpi.js
+++ 10_classes_and_object_op/mathpi.js
@@ -1,5 +1,38 @@
 
-// Object.getOwnPropertyDescriptor(Math)
+const descripter = Object.getOwnPropertyDescriptor(Math,"PI")
 
-console.log(Math.PI);
+// console.log(descripter);
 
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+ 
+// const myNewObject =  Object.create(null)
+
+const chai = {
+    name : "Ginger Chai",
+    price : "250",
+    isAvailavle : true,
+
+    orderChai: function (){
+        console.log("chai nahi bani");
+        
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai , "name"));
+
+Object.defineProperty(chai, "name", {
+    // Writable:false,
+    enumerable: true
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai , "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== "function") {
+        console.log(`${key} : ${value}`);
+    }
+} 
